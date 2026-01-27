24c40af9b15328962f72f168801c7a978b5a525b
pankocatt <133233872+pankocatt@users.noreply.github.com>
2026-01-27 10:31:14 -0500

    Added initial doxygen files (classes need commenting and documentation!)

---
+++ app/doxygen/html/hierarchy.js
@@ -0,0 +1,9 @@
+var hierarchy =
+[
+    [ "FlaskForm", null, [
+      [ "app.LoginForm", "classapp_1_1_login_form.html", null ],
+      [ "app.LogoutForm", "classapp_1_1_logout_form.html", null ],
+      [ "app.NoteForm", "classapp_1_1_note_form.html", null ],
+      [ "app.RegisterForm", "classapp_1_1_register_form.html", null ]
+    ] ]
+];
---
+++ app/doxygen/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "lnr",
+  1: "lnr"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
