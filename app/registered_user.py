6b0b6795aac15044a680d775c92c36b082024442
BenLippert <145207332+Digi-Bomb@users.noreply.github.com>
2026-02-05 10:51:01 -0500

    Fix small errors to push to main

--- app/registered_user.py
+++ app/registered_user.py
@@ -1,20 +1,26 @@
 from typing import List
-from booking import Bookings
+from booking import Booking
 
 
 class RegisteredUser:
-    def __init__(self, user_id: int, username: str, email: str, password: str, first_name: str, last_name: str):
+    def __init__(
+        self,
+        user_id: int,
+        username: str,
+        email: str,
+        password: str,
+        first_name: str,
+        last_name: str,
+    ):
         self.user_id = user_id
         self.username = username
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
-           
+
     def validate_credentials(self, password: str) -> bool:
         return False
-    
-    def get_bookings(self) -> List[Bookings]:
+
+    def get_bookings(self) -> List[Booking]:
         return []
-    
-    
