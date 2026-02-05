2b1fea2d61c19a3e8976bb94410d1da85a6b199b
BenLippert <145207332+Digi-Bomb@users.noreply.github.com>
2026-02-05 11:39:26 -0500

    Test commit

--- app/account/__init__.py
+++
@@ -1,5 +0,0 @@
-from .account_service import AccountService
-from .registered_user import RegisteredUser
-from .unregistered_user import UnregisteredUser
-
-__all__ = ["AccountService", "RegisteredUser", "UnregisteredUser"]
--- app/account/account_service.py
+++ app/account/account_service.py
@@ -1,5 +1,6 @@
 from app.database_connection import DatabaseConnection
 
+
 class AccountService:
     def __init__(self, database: DatabaseConnection):
         self.database = database
