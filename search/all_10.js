1e007ef2a7703581ed6f0f4b244ce4346cd244fe
ColinGreenidge8896 <ColinGreenidge8896@users.noreply.github.com>
2026-02-17 17:12:06 +0000

    deploy: 9cf49ee65dff58f828ba48e358876a094f0e9fc4

--- search/all_10.js
+++ search/all_10.js
@@ -3,5 +3,6 @@ var searchData=
   ['test_0',['test',['../namespaceapp_1_1app.html#a9507063fbc31c2f4ce9bebe4d358ed1c',1,'app::app']]],
   ['time_5fmanager_1',['time_manager',['../classapp_1_1booking_1_1booking__service_1_1BookingService.html#a4308d53b4c6e9f944be0d768e2ae2c50',1,'app::booking::booking_service::BookingService']]],
   ['time_5fmanager_2epy_2',['time_manager.py',['../time__manager_8py.html',1,'']]],
-  ['timemanager_3',['TimeManager',['../classapp_1_1time__manager_1_1TimeManager.html',1,'app::time_manager']]]
+  ['timemanager_3',['TimeManager',['../classapp_1_1time__manager_1_1TimeManager.html',1,'app::time_manager']]],
+  ['true_4',['True',['../namespaceapp_1_1app.html#a39af92d7446ea1a80d2ce2b64bbfec95',1,'app::app']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['index_0',['index',['../namespaceapp_1_1app.html#a54c93b9697673e5749bb5fcc0bd8f9c8',1,'app::app']]]
+  ['index_0',['index',['../namespaceapp_1_1app.html#a54c93b9697673e5749bb5fcc0bd8f9c8',1,'app::app']]],
+  ['inject_5fuser_1',['inject_user',['../namespaceapp_1_1app.html#a50ecb9f38bb522363ee1ae1798a840da',1,'app::app']]]
 ];
